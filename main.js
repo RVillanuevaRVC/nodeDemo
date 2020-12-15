f036c2613690acaaf11f29ee883ae12d01926848
RVillanuevaRVC <75404849+RVillanuevaRVC@users.noreply.github.com>
2020-12-15 17:27:38 +0700

    main.js works now for drive basic api

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ jwtClient.authorize(function (err, tokens)
         } else 
         {
             console.log("Successfully connected!");
+            //console.log(tokens);
         }
     }
 );
@@ -31,7 +32,11 @@ jwtClient.authorize(function (err, tokens)
 const drive = google.drive({version: 'v3', jwtClient/*auth*/});
 //drive.file.get({})
 
-drive.files.list({auth: jwtClient/*,q: "name contains 'RVDemo'"*/}, (err, res) => 
+/*------------------------------------------------------------------>
+//function listFile()
+//the code below works fine
+//drive.files.list({auth: jwtClient,q: "name contains 'RVDemo'"}, (err, res) =>
+drive.files.list({auth: jwtClient}, (err, res) => 
     {
         if (err) 
             return console.log('-->The API returned an error: ' + err);
@@ -49,29 +54,74 @@ drive.files.list({auth: jwtClient/*,q: "name contains 'RVDemo'"*/}, (err, res) =
         }
   }
 );
+//end of list file
+*/
+
+/*------------------------------------------------------------------>
+  //test on download file
+  //Code below works now
+   //https://drive.google.com/file/d/1W9rAgKVi2LrZhFiAVayf_-q1wUWLqV76/view?usp=sharing
+    var varfileId = '1W9rAgKVi2LrZhFiAVayf_-q1wUWLqV76';
+    var dest = fs.createWriteStream('./dl-2.txt');
+
+    drive.files.get({ auth: jwtClient,fileId: varfileId, alt: 'media' })
+        .then(result => 
+            { 
+                console.log(result.data); 
+                dest.write(result.data) ;
+                //console.log
+            })
+        .catch(err=>
+            { 
+                console.log('Erroer happend------------>');
+                console.log(err); 
+            });
+    //end of test download file
+*/
+
 
 /*
-drive.files.list({auth: jwtClient,q: "name contains 'TV'"}, function (err, response) 
-    {
-        if (err) 
-        {
-            console.log('The API returned an error: ' + err);
-            return;
-        }
-
-        var files = response.files;
-        if (files.length == 0) 
-        {
-            console.log('No files found.');
-        } else 
-        {
-            console.log('Files from Google Drive:');
-            for (var i = 0; i < files.length; i++) 
-            {
-                var file = files[i];
-                console.log('%s (%s)', file.name, file.id);
-            }
-        }
-    }
-);
+//------------------------------------------------------------------>
+//test on upload text file
+//Code below works now
+const res = drive.files.create({auth: jwtClient,requestBody: {name: 'DrvTest.txt',
+        mimeType: 'text/plain',
+        parents:['1rMkdKUZYrguYvaG0dXNWR7zVCzMYpZc_']},
+    media: {mimeType: 'text/plain',body: 'Hello World,DrvAPI'}
+}).then (result => 
+    { 
+        console.log(result); 
+        //dest.write(result.data) ;
+        //console.log
+    })
+.catch(err=>
+    { 
+        console.log('Erroer happend------------>');
+        console.log(err); 
+    });
+;
+//console.log(res.data) ;
 */
+
+
+//------------------------------------------------------------------>
+//test on upload Photo file
+//Code below works now
+const res = drive.files.create({auth: jwtClient,requestBody: {name: 'testimage.png',
+        mimeType: 'image/png',
+        parents:['1rMkdKUZYrguYvaG0dXNWR7zVCzMYpZc_']},
+    media: {mimeType: 'image/png',body: fs.createReadStream('awesome.png')}
+}).then (result => 
+    { 
+        console.log(result); 
+        //dest.write(result.data) ;
+        //console.log
+    })
+.catch(err=>
+    { 
+        console.log('Erroer happend------------>');
+        console.log(err); 
+    });
+;
+//console.log(res.data) ;
+
