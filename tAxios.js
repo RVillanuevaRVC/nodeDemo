b9fa1a615b2a84278a82f439bc109ab6bba918d0
RVillanuevaRVC <75404849+RVillanuevaRVC@users.noreply.github.com>
2020-12-16 16:08:37 +0700

    Update tAxios.js

--- tAxios.js
+++ tAxios.js
@@ -1,1 +1,119 @@
 //https://github.com/axios/axios
+
+//below is an examle of using axios to wirk with Cheerio for web scraping
+/*
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+async function fetchHTML(url) {
+  const { data } = await axios.get(url);
+  return cheerio.load(data);
+}
+const $ = await fetchHTML("https://example.com")
+
+// Print the full HTML
+console.log(`Site HTML: ${$.html()}\n\n`)
+
+// Print some specific page content
+console.log(`First h1 tag: ${$('h1').text()}`)
+*/
+
+//below is another example of Axios
+//refer to https://github.com/axios/axios#nodejs
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+// Optionally the request above could also be done as
+axios.get('/user', {
+    params: {
+      ID: 12345
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });  
+
+// Want to use async/await? Add the `async` keyword to your outer function/method.
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+axios.post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+//below is sample for Performing multiple concurrent requests
+
+function getUserAccount() {
+  return axios.get('/user/12345');
+}
+
+function getUserPermissions() {
+  return axios.get('/user/12345/permissions');
+}
+
+Promise.all([getUserAccount(), getUserPermissions()])
+  .then(function (results) {
+    const acct = results[0];
+    const perm = results[1];
+  });
+
+//below shows handling error
+axios.get('/user/12345')
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
+
+//Using toJSON you get an object with more information about the HTTP error.
+axios.get('/user/12345')
+  .catch(function (error) {
+    console.log(error.toJSON());
+  });
+
+//another example 
+//https://attacomsian.com/blog/http-requests-axios
