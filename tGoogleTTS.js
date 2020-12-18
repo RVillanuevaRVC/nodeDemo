48c07f89e3ec5032eb886d328c48adae4fd0596b
RVillanuevaRVC <75404849+RVillanuevaRVC@users.noreply.github.com>
2020-12-18 19:34:58 +0700

    Update tGoogleTTS.js

--- tGoogleTTS.js
+++ tGoogleTTS.js
@@ -1,4 +1,6 @@
+//https://cloud.google.com/text-to-speech/docs/libraries
 //https://cloud.google.com/text-to-speech/?_ga=2.232553494.-889949602.1608294185&_gac=1.83795300.1608294233.Cj0KCQiAw_H-BRD-ARIsALQE_2Nx1-EBb4-yCycAbybro5rA7jqt0zmwCTHvYV0UyJ4m-kjdNXP-lCIaAhaLEALw_wcB#section-2
+//https://www.woolha.com/tutorials/node-js-google-cloud-text-to-speech-api-examples
 //https://github.com/googleapis/nodejs-text-to-speech
 
 //code from the github project nodejs-text-to-speech
